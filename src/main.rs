28a5ad164dd5e0657ba71dd1551ea3f6e3181362
AndreiStreche0 <andreistreche101@gmail.com>
2025-02-19 19:00:52 +0200

    main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+struct Recipe {
+    id: i32,
+    name: String,
+    description: String,
+    ingredients: HashMap<String, String>,
+    instructions: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    
+    let mut recipe_list : Vec<Recipe>;
+
+    while true {
+        println!("What would you like to do?");
+        println!("1.Add recipe");
+        println!("2.View recipe list");
+        println!("3.View recipe details");
+        println!("4.Search recipe");
+        println!("5.Edit recipe");
+        println!("6.Delete recipe");
+        println!("7.Exit!");
+        let mut input = String::new();
+
+        std::io::stdin().read_line(&mut input).expect("Nu am putut citi linia");
+        let mut option: i64 = input.trim().parse().expect("Introdu un număr valid");
+
+        match option {
+            1 => 
+            2 =>
+            3 =>
+            4 =>
+            5 =>
+            6 =>
+            7 => std::process::exit(0),
+            _ => std::process::exit(-1),
+        }
+
+    }
+
 }
